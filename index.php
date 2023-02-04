0361d051fcec40e7a66d97274c5c19c6da4592ec
Abashin Aleksey <mmilasomm@gmail.com>
2023-02-04 18:54:25 +0300

    сделали дз файл php

--- index.php
+++ index.php
@@ -1,47 +1,52 @@
 <?php
-$name = "Alex";
+
+$names = "Alex";
 $prof = "businessman";
 $cityCountry = "Дом на_берегу_моря";
 $email = "vcb1990@rambler.com";
 $phone = "+188843567821";
-$skills = [
+ $works =[
     [
-        'name' => 'здоровье',
-        'percent' => 99,
+        "name" => "хозяин",
+        "dateStart" => "март 2016",
+        "dateEnd" => "текущее время",
+        "description" => "хозяин компании",
     ],
     [
-        'name' => 'успех',
-        'percent' => 98,
+        "name" => "бугалтер",
+        "dateStart" => "март 2014",
+        "dateEnd" => "март 2013",
+        "description" => "считал денюшку",
     ],
     [
-        'name' => 'карьера',
-        'percent' => 97,
+        "name" => "наёмник",
+        "dateStart" => "март 2012",
+        "dateEnd" => "март 2011",
+        "description" => "наблюдатель",
     ],
 ]
-    ?>
-
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Мое резюме</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        html,
-        body,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-family: 'Jost', sans-serif;
+  <title>Мое резюме</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    html,
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: 'Jost', sans-serif;
         }
     </style>
 </head>
@@ -59,7 +64,7 @@ $skills = [
                             style="width:100%" alt="Avatar">
                         <div class="w3-display-bottomleft w3-container w3-text-black">
                             <h2>
-                                <?php echo $name; ?>
+                                <?php echo $names; ?>
                             </h2>
                         </div>
                     </div>
@@ -117,67 +122,55 @@ $skills = [
                 <div class="w3-container w3-card w3-white w3-margin-bottom">
                     <h2 class="w3-text-grey w3-padding-16"><i
                             class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>Front End Developer </b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span
-                                class="w3-tag w3-teal w3-round">Current</span></h6>
-                        <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est
-                            reprehenderit
-                            sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                        <hr>
+                    <?php for ($i = 0; $i < count($works); $i++): ?>
+                        <div class="w3-container">
+                            <h5 class="w3-opacity"><b>
+                                    <?= $works[$i]['name']; ?>
+                                </b></h5>
+                            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?= $works[$i]['dateStart'];?> - <span
+                                    class="w3-tag w3-teal w3-round"><?= $works[$i]['dateEnd'];?></span></h6>
+                            <p><?= $works[$i]['description'];?></p>
+                            <hr>
+                        </div>
+                    <?php endfor; ?>
+                    <div class="w3-container w3-card w3-white">
+                        <h2 class="w3-text-grey w3-padding-16"><i
+                                class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование
+                        </h2>
+                        <div class="w3-container">
+                            <h5 class="w3-opacity"><b>sam.ru</b></h5>
+                            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+                            <p>Web Development! All I need to know in one place</p>
+                            <hr>
+                        </div>
+                        <div class="w3-container">
+                            <h5 class="w3-opacity"><b>London Business School</b></h5>
+                            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015
+                            </h6>
+                            <p>Master Degree</p>
+                            <hr>
+                        </div>
+                        <div class="w3-container">
+                            <h5 class="w3-opacity"><b>School of Coding</b></h5>
+                            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013
+                            </h6>
+                            <p>Bachelor Degree</p><br>
+                        </div>
                     </div>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014
-                        </h6>
-                        <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est
-                            reprehenderit
-                            sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                        <hr>
-                    </div>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012
-                        </h6>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-                    </div>
+                    <!-- End Right Column -->
                 </div>
-
-                <div class="w3-container w3-card w3-white">
-                    <h2 class="w3-text-grey w3-padding-16"><i
-                            class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>gb.ru</b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-                        <p>Web Development! All I need to know in one place</p>
-                        <hr>
-                    </div>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>London Business School</b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-                        <p>Master Degree</p>
-                        <hr>
-                    </div>
-                    <div class="w3-container">
-                        <h5 class="w3-opacity"><b>School of Coding</b></h5>
-                        <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-                        <p>Bachelor Degree</p><br>
-                    </div>
-                </div>
-                <!-- End Right Column -->
+                <!-- End Grid -->
             </div>
-            <!-- End Grid -->
+            <!-- End Page Container -->
         </div>
-        <!-- End Page Container -->
-    </div>
-    <!-- Footer -->
-    <footer class="w3-container w3-teal w3-center w3-margin-top">
-        <p>Find me on social media.</p>
-        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-        <i class="fa fa-twitter w3-hover-opacity"></i>
-        <i class="fa fa-linkedin w3-hover-opacity"></i>
-        <!-- End footer -->
-    </footer>
+        <!-- Footer -->
+        <footer class="w3-container w3-teal w3-center w3-margin-top">
+            <p>Find me on social media.</p>
+            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+            <i class="fa fa-twitter w3-hover-opacity"></i>
+            <i class="fa fa-linkedin w3-hover-opacity"></i>
+            <!-- End footer -->
+        </footer>
 </body>
 
 </html>
