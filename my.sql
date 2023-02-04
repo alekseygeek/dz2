5cc77a1bb43ca47ac1e185cd5efce0473e632cce
Abashin Aleksey <mmilasomm@gmail.com>
2023-02-04 19:42:58 +0300

    сделали дз sql

---
+++ my.sql
@@ -0,0 +1,30 @@
+--  Создать файл my.sql, в котором должна создаваться таблица с информацией об одногруппниках. В таблице должно быть четыре поля: id, name, age, address. Все поля в таблице обязательны для заполнения.
+
+-- Необходимо добавить 5-10 одногруппников в данную таблицу.
+
+-- Необходимо написать запрос на получение имен всех одногруппников (только имен, без всего остального), которые живут в Москве и их возраст находится в диапазоне [18, 30) лет.
+
+-- Примечание:
+
+-- Квадратные скобки при указании диапазона, означают "включительно", а круглые "не включительно", то есть диапазон (7, 9] означает "от 7, где 7 не попадает в данный диапазон, до 9 включительно". Такой синтаксис нельзя использовать в sql, вам нужно найти решение, как такое условие можно записать в sql по-другому.
+
+-- Для проверок работы своего скрипта можете использовать свою базу данных, либо используйте сайт: https://onecompiler.com/mysql
+
+CREATE TABLE EMPLOYEE (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age TEXT NOT NULL,
+  city TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Clark', '12','Tula');
+INSERT INTO EMPLOYEE VALUES (0002, 'Dave', '18','Moscow');
+INSERT INTO EMPLOYEE VALUES (0004, 'Aba', '56','Tulsa');
+INSERT INTO EMPLOYEE VALUES (0005, 'Avasa', '29','Moscow');
+INSERT INTO EMPLOYEE VALUES (0006, 'Avdasa', '33','Baku');
+INSERT INTO EMPLOYEE VALUES (0007, 'Azereva', '7','Moscow');
+INSERT INTO EMPLOYEE VALUES (0008, 'Amreva', '78','Moscow');
+
+-- fetch 
+SELECT * FROM EMPLOYEE WHERE city = 'Moscow' AND age < 30 AND age > 17;
